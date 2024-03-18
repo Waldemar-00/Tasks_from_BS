f9775df4d6549e9b01e0ea4a146c2cebf101d345
ULADZIMIR MARTYNCHYK <01myit@gmail.com>
2024-03-18 21:14:48 +0300

    Task: 64. Hosting in JS. Declaration and initializtion!

---
+++ hoisting_64.js
@@ -0,0 +1,26 @@
+/** ЗАДАЧА 64 - Hoisting
+ *
+ * Измените код, чтобы исправить ошибку после строки 13.
+ *
+ * Ошибка после строки 21 все равно должна генерироваться.
+ * Почему ошибка после строки 21 возникает?
+ */
+//! https://www.w3schools.com/js/js_hoisting.asp  !!!
+
+const a = 5
+const b = 10
+//! console.log(c) - undefined! WITH VAR
+if (b > a) {
+  // var c = 2
+  let c = 2
+  c = a + b + c
+  // ДО: ReferenceError: Cannot access 'c' before initialization
+  // ПОСЛЕ: Нет ошибки
+  console.log(c)
+  // 17
+}
+
+console.log(c)
+// Uncaught ReferenceError: c is not defined
+
+//! https://www.w3schools.com/js/js_hoisting.asp  !!!
